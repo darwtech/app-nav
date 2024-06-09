0cbc2d7cdd7c36db19b01bd5f879b20a932eb9f4
Darwin Chavez <69565311+darwtech@users.noreply.github.com>
2024-06-09 15:00:02 -0500

    Update conex.ts

--- database/conex.ts
+++ database/conex.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnuHLmO-96-fffy93-_qjkXzcTCrb0cDA",
-  authDomain: "app-nav-cc3d5.firebaseapp.com",
-  projectId: "app-nav-cc3d5",
-  storageBucket: "app-nav-cc3d5.appspot.com",
-  messagingSenderId: "80722447318",
-  appId: "1:80722447318:web:fb634c7fefbbcb79337277",
-  measurementId: "G-PYN34ENGQ5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Inicializar Firebase
